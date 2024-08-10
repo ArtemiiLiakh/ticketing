a64236fd0a7e4cf1184d63341e64c143d3117774
ArtemLyah <asuskonotopo@gmail.com>
2024-08-10 23:42:38 +0300

    updated auth version

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -14,7 +14,7 @@ const connectDatabase = async () => {
 app.listen(5000, async () => {
   console.clear();
   await connectDatabase();
-  console.log('v0.0.6');
+  console.log('v0.0.8');
   console.log(config.CLIENT_URL);
   console.log('Started on http://localhost:5000')
 });
